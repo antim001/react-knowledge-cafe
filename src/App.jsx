51c48413f7cf0233211e04f5800cd18ea9650282
Antim <you@example.com>
2024-01-07 23:18:31 +0600

    Header and Blogs components are created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
  
   return (
     <>
       
-      <h1 className='text-6xl bg-red-300'> React knowledge cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
       
       
     </>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+           <h1 className='text-6xl '> React knowledge cafe</h1> 
+        </div>
+    );
+};
+
+export default Header;
